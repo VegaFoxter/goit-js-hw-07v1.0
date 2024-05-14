37e8d543f4f748524995cbd09ce1ebec4008db6d
VegaFoxter <omelyanchuk05victor@gmail.com>
2024-05-14 21:40:44 +0300

    1

---
+++ task-1.js
@@ -0,0 +1,7 @@
+const categoriesList = document.querySelectorAll(".item");
+console.log("Number of categories: ", categoriesList.length);
+
+categoriesList.forEach((category) => {
+  console.log("Category: ", category.firstElementChild.textContent);
+      console.log("Elements: ", category.lastElementChild.children.length);
+});
